2bbe2993f5441c16bcad7b65ba171e9d1e912eba
Rowlling <1768053461@qq.com>
2021-05-25 20:28:46 +0800

    2-7 增加hello的测试接口test.http

--- src/main/java/com/zhuanye/wiki/config/WikiApplication.java
+++ src/main/java/com/zhuanye/wiki/config/WikiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-@ComponentScan("com.zhuanye")//因为该文件不在controller下，与其是并列关系，不在@RestController的扫描范围内，因此需要手动导入
+@ComponentScan("com.zhuanye")//因为ComponentScan类只扫描该类下的子包，该文件不在controller下，与其是并列关系，不在@RestController的扫描范围内，因此需要手动导入
 //@ComponentScan({"com.zhuanye","com.test"})//扫描多个包
 @SpringBootApplication
 public class WikiApplication {
--- src/main/java/com/zhuanye/wiki/controller/TestController.java
+++ src/main/java/com/zhuanye/wiki/controller/TestController.java
@@ -2,8 +2,11 @@ package com.zhuanye.wiki.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController//返回字符串
 //@Controller//返回页面
@@ -15,7 +18,7 @@ public class TestController {
     DELETE删除
     /user/1 访问id=1的user即传统方式 /user?id=1
     */
-   // @PostMapping
+   // @PostMapping 浏览器访问不到，因为浏览器不接受POST请求
    // @PutMapping
    // @DeleteMapping
    // @RequestMapping(value = "/user/1", method = RequestMethod.GET)
@@ -24,6 +27,12 @@ public class TestController {
     //@RequestMapping("/hello")//http://127.0.0.1:18080/hello
     @GetMapping("/hello")
     public String hello(){
-        return "Hello Word!";
+        return "Hello World!";
     }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! post,"+ name;
+    }
+
 }
