94f649ecb6ef06294eba5bd1c62014abb5eabdcd
Rowlling <1768053461@qq.com>
2021-05-28 16:28:33 +0800

    3-5-3 增加demo表列表查询

---
+++ src/main/java/com/zhuanye/wiki/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.zhuanye.wiki.controller;
+
+import com.zhuanye.wiki.domain.Demo;
+import com.zhuanye.wiki.service.DemoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController//返回字符串
+//@Controller//返回页面
+
+@RequestMapping("/demo")
+public class DemoController {
+    @Resource
+    private DemoService demoService;
+
+    @GetMapping("/list")
+    public List<Demo> list(){
+        return demoService.list();
+    }
+}
