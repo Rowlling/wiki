bcc4ae5c37541d8736dd62dabb4a1eaa81141bcf
Rowlling <1768053461@qq.com>
2021-05-28 20:20:36 +0800

    3-6-3 增加CommonResponse,以后所有后端接口的返回值全部都是增加CommonResponse类型

--- src/main/java/com/zhuanye/wiki/controller/EbookController.java
+++ src/main/java/com/zhuanye/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.zhuanye.wiki.controller;
 
 import com.zhuanye.wiki.domain.Ebook;
+import com.zhuanye.wiki.resp.CommonResp;
 import com.zhuanye.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,11 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
 
