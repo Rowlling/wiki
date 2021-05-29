b0c6c7eb51e37eedd6c2f564374127e4ffcb75d1
Rowlling <1768053461@qq.com>
2021-05-29 21:33:32 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/zhuanye/wiki/controller/EbookController.java
+++ src/main/java/com/zhuanye/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.zhuanye.wiki.controller;
 
-import com.zhuanye.wiki.domain.Ebook;
+import com.zhuanye.wiki.req.EbookReq;
 import com.zhuanye.wiki.resp.CommonResp;
+import com.zhuanye.wiki.resp.EbookResp;
 import com.zhuanye.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +20,13 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
+
         return resp;
+
     }
 }
 
